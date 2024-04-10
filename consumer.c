8e1d21f134c87a558236f7aad703e22c300ca976
Jayson Dmello <110573042+jaysonjdmello@users.noreply.github.com>
2024-04-10 15:31:50 -0600

    add consumer change 3

--- consumer.c
+++ consumer.c
@@ -3,42 +3,17 @@
 
 #include "common.c"
 
-void basic_consume_loop(rd_kafka_t *rk) {
-  rd_kafka_resp_err_t err;
+static volatile sig_atomic_t run = 1;
 
-  // Create a list of topics to subscribe to
-  rd_kafka_topic_partition_list_t *subscription;
-
-  subscription = rd_kafka_topic_partition_list_new(1);
-
-  // Assuming we'll subscribe to one topic for now
-  rd_kafka_topic_partition_list_add(
-      subscription, "data-plane-perf",
-      RD_KAFKA_PARTITION_UA); // UA = Unassigned partition
-
-  if ((err = rd_kafka_subscribe(rk, subscription))) {
-    fprintf(stderr, "%% Failed to start consuming topics: %s\n",
-            rd_kafka_err2str(err));
-    exit(1);
-  }
-
-  while (1) {
-    rd_kafka_message_t *rkmessage = rd_kafka_consumer_poll(rk, 500);
-    if (rkmessage) {
-      rd_kafka_message_destroy(rkmessage);
-    }
-  }
-
-  err = rd_kafka_consumer_close(rk);
-  if (err)
-    fprintf(stderr, "%% Failed to close consumer: %s\n", rd_kafka_err2str(err));
-  else
-    fprintf(stderr, "%% Consumer closed\n");
-}
+/**
+ * @brief Signal termination of program
+ */
+static void stop(int sig) { run = 0; }
 
 int main(int argc, char **argv) {
   rd_kafka_t *consumer;
   rd_kafka_conf_t *conf;
+  rd_kafka_resp_err_t err;
   char errstr[512];
 
   // Parse the command line.
@@ -62,18 +37,79 @@ int main(int argc, char **argv) {
   // Load the relevant configuration sections.
   conf = rd_kafka_conf_new();
   load_config_group(conf, key_file, "default");
+  load_config_group(conf, key_file, "consumer");
 
-  // Create the Producer instance.
+  // Create the Consumer instance.
   consumer = rd_kafka_new(RD_KAFKA_CONSUMER, conf, errstr, sizeof(errstr));
   if (!consumer) {
-    g_error("Failed to create new producer: %s", errstr);
+    g_error("Failed to create new consumer: %s", errstr);
     return 1;
   }
+  rd_kafka_poll_set_consumer(consumer);
 
   // Configuration object is now owned, and freed, by the rd_kafka_t instance.
   conf = NULL;
 
-  basic_consume_loop(consumer);
+  // Convert the list of topics to a format suitable for librdkafka.
+  const char *topic = "data-plane-perf";
+  rd_kafka_topic_partition_list_t *subscription =
+      rd_kafka_topic_partition_list_new(1);
+  rd_kafka_topic_partition_list_add(subscription, topic, RD_KAFKA_PARTITION_UA);
+
+  // Subscribe to the list of topics.
+  err = rd_kafka_subscribe(consumer, subscription);
+  if (err) {
+    g_error("Failed to subscribe to %d topics: %s", subscription->cnt,
+            rd_kafka_err2str(err));
+    rd_kafka_topic_partition_list_destroy(subscription);
+    rd_kafka_destroy(consumer);
+    return 1;
+  }
+
+  rd_kafka_topic_partition_list_destroy(subscription);
+
+  // Install a signal handler for clean shutdown.
+  signal(SIGINT, stop);
+
+  // Start polling for messages.
+  while (run) {
+    rd_kafka_message_t *consumer_message;
+
+    consumer_message = rd_kafka_consumer_poll(consumer, 500);
+    if (!consumer_message) {
+      g_message("Waiting...");
+      continue;
+    }
+
+    if (consumer_message->err) {
+      if (consumer_message->err == RD_KAFKA_RESP_ERR__PARTITION_EOF) {
+        /* We can ignore this error - it just means we've read
+         * everything and are waiting for more data.
+         */
+      } else {
+        g_message("Consumer error: %s",
+                  rd_kafka_message_errstr(consumer_message));
+        return 1;
+      }
+    } else {
+#ifdef DEBUG
+      g_message("Consumed event from topic %s: key = %.*s value = %s",
+                rd_kafka_topic_name(consumer_message->rkt),
+                (int)consumer_message->key_len, (char *)consumer_message->key,
+                (char *)consumer_message->payload);
+#endif
+    }
+
+    // Free the message when we're done.
+    rd_kafka_message_destroy(consumer_message);
+  }
+
+  // Close the consumer: commit final offsets and leave the group.
+  g_message("Closing consumer");
+  rd_kafka_consumer_close(consumer);
+
+  // Destroy the consumer.
+  rd_kafka_destroy(consumer);
 
   return 0;
 }
